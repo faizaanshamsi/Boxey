44cb9a97bd88397294e8fbd2c357b0ee29332655
faizaanshamsi <faizaanshamsi@gmail.com>
2013-12-15 17:42:28 -0500

    add dynamic labels to boxes

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -7,6 +7,8 @@ class AppDelegate
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview(@blue_view)
 
+    add_labels_to_boxes
+
     @add_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @add_button.setTitle("Add", forState:UIControlStateNormal)
     @add_button.sizeToFit
@@ -43,12 +45,11 @@ class AppDelegate
         last_view.frame.origin.y + last_view.frame.size.height + 10],
         [last_view.frame.size.width + 10, 10 + last_view.frame.size.height])
     @window.insertSubview(new_view, atIndex:0)
+    add_labels_to_boxes
   end
 
   def remove_tapped
-    other_views = @window.subviews.select do |view|
-      not view.is_a? UIButton
-    end
+    other_views = self.boxes
     @last_view = other_views.last
 
     if @last_view and other_views.count > 1
@@ -67,7 +68,78 @@ class AppDelegate
 
           completion:lambda { |finished|
             @last_view.removeFromSuperview
+            add_labels_to_boxes
           })
     end
   end
+
+  def add_label_to_box(box)
+    box.subviews.each do |subview|
+      subview.removeFromSuperview
+    end
+
+    index_of_box = @window.subviews.index(box)
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = "#{index_of_box}"
+    label.textColor = UIColor.whiteColor
+    label.backgroundColor = UIColor.clearColor
+    label.sizeToFit
+    label.center = [box.frame.size.width / 2, box.frame.size.height / 2]
+    box.addSubview(label)
+  end
+
+  def boxes
+    @window.subviews.select do |view|
+      not (view.is_a?(UIButton) or view.is_a?(UILabel))
+    end
+  end
+
+  def add_labels_to_boxes
+    self.boxes.each do |box|
+      add_label_to_box(box)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
